bb2ad5cc2a958ed0cd55a0be27a8652006ece2a1
Md Ashiqur Rahman Pranto <59225534+ashiqurrahman696@users.noreply.github.com>
2024-04-14 08:58:57 +0600

    Updated script.js

--- script.js
+++ script.js
@@ -1,11 +1,31 @@
+let board = document.querySelector('.board');
 let pops = document.querySelectorAll('.pop');
 let popSound = new Audio('pop.mp3');
+let flipH = document.getElementById('flipH');
+let flipV = document.getElementById('flipV');
 
 pops.forEach(pop => {
-    pop.addEventListener('click', function(){
+    pop.addEventListener('click', ()=>{
         pop.classList.toggle('active');
         popSound.pause();
         popSound.currentTime = 0;
         popSound.play();
     });
+});
+
+flipH.addEventListener('click', ()=>{
+    board.classList.toggle('flipH');
+    setTimeout(()=>{
+        pops.forEach(pop => {
+            pop.classList.toggle('active');
+        });
+    }, 200);
+});
+flipV.addEventListener('click', ()=>{
+    board.classList.toggle('flipV');
+    setTimeout(()=>{
+        pops.forEach(pop => {
+            pop.classList.toggle('active');
+        });
+    }, 200);
 });
